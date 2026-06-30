a38cd9a57ab63dfa72b126835a413b9efb0c93bc
ajeet11ims-prog <ajeet11ims@gmail.com>
2026-07-01 00:19:13 +0530

    Update tests list: sici Test 7

--- tests-data.js
+++ tests-data.js
@@ -1149,6 +1149,19 @@ window.CAT_TESTS = [
     ],
     "minutes": 60,
     "questions": 25
+  },
+  {
+    "title": "sici Test 7",
+    "file": "tests/Topic Test/QA/Arithmetic/4.SI & CI/SICI Test -7.html",
+    "original": "SICI Test -7.html",
+    "folders": [
+      "Topic Test",
+      "QA",
+      "Arithmetic",
+      "4.SI & CI"
+    ],
+    "minutes": 60,
+    "questions": 25
   }
 ];
 
