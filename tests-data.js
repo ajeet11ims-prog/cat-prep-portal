b5d7d0488a3d5783dd96d41e0652913b1dbf2b59
ajeet11ims-prog <ajeet11ims@gmail.com>
2026-07-03 01:45:26 +0530

    Update tests list: Arithmetic Part -I ( Without TSD & TW )-9

--- tests-data.js
+++ tests-data.js
@@ -1447,6 +1447,17 @@ window.CAT_TESTS = [
     ],
     "minutes": 60,
     "questions": 25
+  },
+  {
+    "title": "Arithmetic Part -I ( Without TSD & TW )-9",
+    "file": "tests/Area Wise Test/QA/Arithmetic/Arithmetic Part -I ( Without TSD & TW )-9.html",
+    "original": "Arithmetic Part -I ( Without TSD & TW )-9.html",
+    "folders": [
+      "Area Wise Test",
+      "QA",
+      "Arithmetic"
+    ],
+    "minutes": 60
   }
 ];
 
