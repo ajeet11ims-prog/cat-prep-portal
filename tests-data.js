9805db0adf24b2d60c7ea9b926a2736b39669bc5
ajeet11ims-prog <ajeet11ims@gmail.com>
2026-07-21 00:34:06 +0530

    Update tests list: LRDI CAT 2018 SLOT -1 Set -7

--- tests-data.js
+++ tests-data.js
@@ -2449,6 +2449,19 @@ window.CAT_TESTS = [
     ],
     "minutes": 15,
     "questions": 4
+  },
+  {
+    "title": "LRDI CAT 2018 SLOT -1 Set -7",
+    "file": "tests/PYQ/Year Wise/LRDI/2018/LRDI CAT 2018 SLOT -1 Set -7.html",
+    "original": "LRDI CAT 2018 SLOT -1 Set -7.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2018"
+    ],
+    "minutes": 15,
+    "questions": 4
   }
 ];
 
