7dfa7995292c0a3eb92e3ac8c4c41b5423c0ea0c
ajeet11ims-prog <ajeet11ims@gmail.com>
2026-08-09 19:29:47 +0530

    Update tests list: bulk upload 12 tests

--- tests-data.js
+++ tests-data.js
@@ -2040,6 +2040,162 @@ window.CAT_TESTS = [
     "questions": 5
   },
   {
+    "title": "LRDI CAT 2023 SLOT -1 Set -1",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -1 Set -1.html",
+    "original": "LRDI CAT 2023 SLOT -1 Set -1.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -1 Set -2",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -1 Set -2.html",
+    "original": "LRDI CAT 2023 SLOT -1 Set -2.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -1 Set -3",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -1 Set -3.html",
+    "original": "LRDI CAT 2023 SLOT -1 Set -3.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -1 Set -4",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -1 Set -4.html",
+    "original": "LRDI CAT 2023 SLOT -1 Set -4.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -2 Set -1",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -2 Set -1.html",
+    "original": "LRDI CAT 2023 SLOT -2 Set -1.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -2 Set -2",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -2 Set -2.html",
+    "original": "LRDI CAT 2023 SLOT -2 Set -2.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -2 Set -3",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -2 Set -3.html",
+    "original": "LRDI CAT 2023 SLOT -2 Set -3.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -2 Set -4",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -2 Set -4.html",
+    "original": "LRDI CAT 2023 SLOT -2 Set -4.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -3 Set -1",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -3 Set -1.html",
+    "original": "LRDI CAT 2023 SLOT -3 Set -1.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -3 Set -2",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -3 Set -2.html",
+    "original": "LRDI CAT 2023 SLOT -3 Set -2.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -3 Set -3",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -3 Set -3.html",
+    "original": "LRDI CAT 2023 SLOT -3 Set -3.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
+    "title": "LRDI CAT 2023 SLOT -3 Set -4",
+    "file": "tests/PYQ/Year Wise/LRDI/2023/LRDI CAT 2023 SLOT -3 Set -4.html",
+    "original": "LRDI CAT 2023 SLOT -3 Set -4.html",
+    "folders": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
+    ],
+    "minutes": 15,
+    "questions": 5
+  },
+  {
     "title": "DI - Bar Graph - Test - 11",
     "file": "tests/Topic Test/LRDI/DI - Bar Graph - Test - 11.html",
     "original": "DI - Bar Graph - Test - 11.html",
@@ -5991,6 +6147,15 @@ window.CAT_FOLDERS = [
       "Year Wise",
       "LRDI",
       "2022"
+    ]
+  },
+  {
+    "name": "2023",
+    "path": [
+      "PYQ",
+      "Year Wise",
+      "LRDI",
+      "2023"
     ]
   },
   {
